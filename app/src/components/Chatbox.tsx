9579913204da3c8ccb0519aa6614f5f2acabd0b5
unfiltered-syrup <jw6058@nyu.edu>
2023-06-10 11:57:36 +0800

    major fix

--- app/src/components/Chatbox.tsx
+++ app/src/components/Chatbox.tsx
@@ -3,53 +3,58 @@ import { useState } from "react";
 import ReactDOM from "react-dom/client";
 
 //holds message data
-interface Message {
-    sender: string | undefined;
-    message: string | undefined;
-}
-
 function Chatbox(){
-    const [convo, setConvo] = useState<Message[]>([]);
-    const [newmsg, setNewsmg] = useState<Message>({sender: 'bot', message: 'first message'});
-    fetch('/api/fetch_questions',{
-        method: 'post',
-        credentials: "include",
-        headers:{
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            answers: ['yes', 'no']
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        if(data.quesion){
-            setNewsmg({sender: 'bot', message: data.question})
-        }
-        else{
-            setNewsmg({sender: 'bot', message: 'You are all set.'})
-        }
-    })
+    const [question, setQuestion] = useState<string>("");
+    const [questionId, setQuestionId] = useState<string>("");
+    const [answer, setAnswer] = useState<string>("");
+
+    const buttonPressed = (event: React.MouseEvent<HTMLButtonElement>) => {
+        const buttonContent = (event.target as HTMLElement).innerText;
+        setAnswer(buttonContent);
+        sendAnswer();
+        console.log(buttonContent);
+
+    }
+
+    const sendAnswer = () => {
+        fetch('http://localhost:5000/api/get_response',{
+            method: 'post',
+            credentials: "include",
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                answer: answer,
+                question_id: questionId,
+            }),
+            })
+            .then(response => response.json())
+            .then(data => {
+                setQuestion(data.question)
+                setQuestionId(questionId + ":" + answer);
+            })
+            .catch((error) =>{
+                console.error('Error', error)
+            });
+    }
+
 
     return(
         <>
         <div className="flex flex-col h-screen p-6 bg-light-pink">
             <div className="flex flex-col mx-10 h-2/3 mb-4 p-3 bg-slate-50 bg-opacity-50 rounded-lg overflow-auto">
                 <div className="mb-3 p-2 rounded-md bg-slate-200">
-                <p>John: {newmsg.message} </p>
+                <p>Gorden Ramsey: {question} </p>
                 </div>
                 <div className="mb-3 p-2 rounded-md bg-green-200 text-right">
-                <p>You: Hello John!</p>
+                <p>You: Hello Gorden!</p>
                 </div>
 
             </div>
 
             <div className="flex items-center mx-10 h-1/3 p-3 bg-slate-50 bg-opacity-50 rounded-lg">
-                <button className="mr-2 py-2 px-4 rounded-md bg-blue-500 text-white hover:bg-blue-600">Button 1</button>
-                <button className="mr-2 py-2 px-4 rounded-md bg-blue-500 text-white hover:bg-blue-600">Button 2</button>
-                <button className="py-2 px-4 rounded-md bg-blue-500 text-white hover:bg-blue-600">Button 3</button>
-            </div>
+                <button onClick={buttonPressed} className="mr-2 py-2 px-4 rounded-md bg-blue-500 text-white hover:bg-blue-600">Yes</button>
+                <button onClick={buttonPressed} className="mr-2 py-2 px-4 rounded-md bg-blue-500 text-white hover:bg-blue-600">No</button>            </div>
             </div>
         </>
     )
--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ from flask_session import Session
 from dotenv import load_dotenv
 import json
 from conversation_flow import tree
-
+import conversation_flow
 # for register
 import re
 
@@ -185,6 +185,60 @@ def get_cuisine_list():
     data = [dict(row) for row in cuisines]
     return [item['cuisineType'] for item in data]
 
+
+@app.route("/api/get_response", methods=["POST"])
+def next_question():
+    if request.method == 'POST':
+        data = request.get_json()
+        answer = data['answer']
+        #a string that represents the path in the conversation flow tree
+        question_id = data['question_id']
+        #answer provided by the user
+        user_response = request.json['answer'] 
+        bot_response = conversation_flow.make_decision(answer ,question_id)
+        response = make_response(jsonify({"question": bot_response}))
+        return response
+    
+@app.route("/api/fetch_user_preference", methods = ['POST'])
+def fetch_user_preference():
+    current_username = request.json['user_name']
+    conn = get_db('Preference')
+    cur = conn.cursor()
+    cur.execute("SELECT diet_preference FROM user_preference WHERE user_name = ?", (current_username,))
+    result = cur.fetchone()
+    conn.close()
+    if result:
+        diet_preference = result[0] # the diet_preference column
+        print(result)
+        response = make_response(jsonify({"success": 'True', "data": diet_preference}))
+        return response
+    # if not found
+    print("user_name not found")
+    response = make_response(jsonify({"success": 'False', "data": None}))
+    return response
+
+@app.route("/api/update_user_preference", methods = ['POST'])
+def update_user_preference():
+    if request.method == 'POST':
+        # assuming the request contains user_name and diet_preference
+        name = request.json['user_name']
+        diet_preference = request.json['diet_preference']
+        conn = get_db('Preference')
+        cur = conn.cursor()
+
+
+        try:
+            cur.execute("UPDATE user_preference SET diet_preference = ? WHERE user_name = ?", (diet_preference, name))
+            cur.commit()
+            conn.close()
+            response = make_response(jsonify({"success": 'True'}))
+            return response
+        except:
+            conn.rollback()
+        conn.close()
+    response = make_response(jsonify({"success": 'False'}))
+    return response
+
 @app.route("/api/fetch_questions")
 def fetch_questions():
     data = request.get_json()
@@ -196,6 +250,54 @@ def fetch_questions():
         return response
     else:
         return jsonify({"result": node})
+    
+def convert_string_to_numbers(num_str):
+    num_list = [int(num) for num in num_str.split()]
+    return num_list
+    
+@app.route("/api/generate_recommended_recipes", methods = ['POST'])
+def generate_recommended_recipes():
+    print("here")
+    if request.method == 'POST':
+        #preference_vector = request.json['preference_vector']
+        # testing 
+        preference_vector = user_to_user.n_generate(1,1000)[0]
+        # extract the preference vectors from the database
+        conn = get_db('test_preference')
+        cur = conn.cursor()
+        cur.execute("SELECT preference_vector FROM users LIMIT 1000")
+        rows = cur.fetchall()
+        conn.close()
+
+        str_preference_vectors = [row[0] for row in rows]
+        print(str_preference_vectors[0])
+        preference_vectors = []
+        for spv in str_preference_vectors:
+            preference_vectors.append(convert_string_to_numbers(spv))
+
+        # replace the none values
+        preference_vector = user_to_user.user_to_user(preference_vector, preference_vectors)
+        
+        # extract the recipes from the database
+        conn = get_db('test_recipe')
+        cur = conn.cursor()
+        cur.execute("SELECT id, preference_vector FROM recipes")
+        rows = cur.fetchall()
+        conn.close()
+        # extract the data into a dictionary
+        recipes = {}
+        for row in rows:
+            recipe_id = row[0]
+            recipe_pref_vec = row[1]
+            recipes[recipe_id] = convert_string_to_numbers(recipe_pref_vec)
+
+        # calculate the top 20 recipes
+        recommended_recipes = user_product.user_product(preference_vector, recipes)
+        print(recommended_recipes) # for test
+        response = make_response(jsonify({"success": 'True', "data": recommended_recipes}))
+        return response
+    response = make_response(jsonify({"success": 'False'}))
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
