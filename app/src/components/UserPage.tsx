4da852c6d07d8925d154b824f94010da696c0e58
Phoebe Wang <kw3036@columbia.edu>
2023-06-10 12:19:21 +0800

    add both shell file, edit UserPage.tsx

--- app/src/components/UserPage.tsx
+++ app/src/components/UserPage.tsx
@@ -10,9 +10,16 @@ interface UserProps {
 const UserPage = (props: UserProps) => {
     const [loggedIn, setLoggedIn] = useState<string | null>(localStorage.getItem('loggedIn'));
     const [localUserData, setUserData] = useState<any>(props.userData);
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
     console.log(localUserData)
     const [localUserPreference, setUserPreference] = useState('');
     console.log(localUserData);
+
+    const toggleModal = () => {
+        setIsModalVisible(!isModalVisible);
+    };
 
     fetch("http://localhost:5000/api/fetch_user_preference", {
         method: "POST",
@@ -57,21 +64,85 @@ const UserPage = (props: UserProps) => {
                 <div className='flex m-10 w-full p-10'>
                     <h1 className='font-UI text-3xl mr-20 my-auto'>Preferences:</h1>
                     <div className='grid-cols-1 space-y-4 mt-10'>
-                        <div className='font-UI shadow-md text-gray-900 bg-gradient-to-r from-light-white to-light-green hover:bg-gradient-to-l hover:from-mid-green hover:to-light-white focus:ring-4 focus:outline-none focus:ring-light-white dark:focus:ring-light-green hover:shadow-lg rounded-lg px-5 py-2.5 text-center mr-2 mb-2'>
-                            <Link to='../accountpreference' className='font-UI text-lg'>Change Account Information</Link>
+                        <div className='font-UI shadow-md text-gray-900 bg-gradient-to-r from-light-white to-light-green hover:bg-gradient-to-l hover:from-mid-green hover:to-light-white focus:ring-4 focus:outline-none focus:ring-light-white hover:shadow-lg rounded-lg px-5 py-2.5 text-center mr-2 mb-2'>
+                            <button
+                                className='font-UI text-lg'
+                                data-modal-target="UsernameModal"
+                                data-modal-toggle="UsernameModal"
+                                data-modal-show="UsernameModal"
+                                type="button"
+                                onClick={toggleModal}>
+                                Change Username
+                            </button>
                         </div>
-                        <div className=' font-UI shadow-md text-gray-900 bg-gradient-to-r from-light-white to-light-green hover:bg-gradient-to-l hover:from-mid-green hover:to-light-white focus:ring-4 focus:outline-none focus:ring-light-white dark:focus:ring-light-green hover:shadow-lg rounded-lg px-5 py-2.5 text-center mr-2 mb-2'>
+                        <div className=' font-UI shadow-md text-gray-900 bg-gradient-to-r from-light-white to-light-green hover:bg-gradient-to-l hover:from-mid-green hover:to-light-white focus:ring-4 focus:outline-none focus:ring-light-white hover:shadow-lg rounded-lg px-5 py-2.5 text-center mr-2 mb-2'>
                             <Link to='../dietpreference' className='font-UI text-lg'>Change Diet Preference </Link>
                         </div>
-                        <div className=' font-UI shadow-md text-gray-900 bg-gradient-to-r from-light-white to-light-green hover:bg-gradient-to-l hover:from-mid-green hover:to-light-white focus:ring-4 focus:outline-none focus:ring-light-white dark:focus:ring-light-green hover:shadow-lg rounded-lg px-5 py-2.5 text-center mr-2 mb-2'>
+                        <div className=' font-UI shadow-md text-gray-900 bg-gradient-to-r from-light-white to-light-green hover:bg-gradient-to-l hover:from-mid-green hover:to-light-white focus:ring-4 focus:outline-none focus:ring-light-white hover:shadow-lg rounded-lg px-5 py-2.5 text-center mr-2 mb-2'>
                             <Link to='../app-preference' className='font-UI text-lg'>Change App Settings</Link>
                         </div>
                         <CuisineButton cuisineName='Italian' />
                     </div>
                 </div>
-
             </div>
         </div>
+        {isModalVisible && (
+            <>
+                <div
+                    className="fixed top-0 left-0 right-0 bottom-0 bg-black opacity-50 z-40"
+                ></div>
+                <div
+                    id="UsernameModal"
+                    aria-hidden="true"
+                    className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center z-50"
+                >
+                    <div className="relative w-full max-w-md max-h-full">
+                        <div className="relative bg-white rounded-lg shadow">
+                            <div className="flex items-start justify-between p-4 rounded-t">
+                                <h3 className="font-UI text-lg flex items-center justify-center">Change Username</h3>
+                                <button
+                                    type="button"
+                                    className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
+                                    onClick={toggleModal} // Added event handler to hide the modal
+                                >
+                                    <svg
+                                        aria-hidden="true"
+                                        className="w-5 h-5"
+                                        fill="currentColor"
+                                        viewBox="0 0 20 20"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                            clipRule="evenodd"
+                                        ></path>
+                                    </svg>
+                                    <span className="sr-only">Close modal</span>
+                                </button>
+                            </div>
+                            <div className="px-5">
+                                <div>
+                                    <label className="font-UI text-sm">Old Username</label>
+                                    <input className="bg-pink-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dark-green focus:border-dark-green block w-full p-2.5" placeholder="Jane Doe" required />
+                                </div>
+                            </div>
+                            <div className="px-5 py-5">
+                                <div>
+                                    <label className="font-UI text-sm">New Username</label>
+                                    <input placeholder="June Dou" className="bg-pink-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dark-green focus:border-dark-green block w-full p-2.5" required />
+                                </div>
+                            </div>
+                            <div className="flex justify-center">
+                            <button className="w-200 text-black bg-pink-100 hover:bg-pink-200 font-UI rounded-lg text-sm px-5 py-2.5 text-center m-5">submit</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>    
+
+    </>
+        )}
+
     </>
 };
 
