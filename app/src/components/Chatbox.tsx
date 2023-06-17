fc6b3e45348f59cd6a88405e4c0f0a0549bb61c6
Phoebe Wang <kw3036@columbia.edu>
2023-06-17 15:30:34 +0800

    modify the chatbox page

--- app/src/components/Chatbox.tsx
+++ app/src/components/Chatbox.tsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import { useEffect, useRef } from "react";
 
 //holds message data
-function Chatbox(){
+function Chatbox() {
     const [question, setQuestion] = useState<string>("");
     const [questionId, setQuestionId] = useState<string>("");
     const [answer, setAnswer] = useState<string>("");
@@ -18,8 +18,8 @@ function Chatbox(){
             record_response();
         }
     }, [clickCount]);
-    
-    const buttonPressed =  async (e: React.MouseEvent<HTMLButtonElement>) => {
+
+    const buttonPressed = async (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
         setClickCount(clickCount + 1);
         const buttonContent = (e.target as HTMLElement).innerText;
@@ -29,54 +29,88 @@ function Chatbox(){
         console.log('button pressed');
     };
 
-    function record_response(){
-        fetch('http://localhost:5000/api/get_response',{
+    function record_response() {
+        fetch('http://localhost:5000/api/get_response', {
             method: 'post',
             credentials: "include",
-            headers:{
+            headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
                 answer: answer,
                 question_id: questionId,
             }),
-            })
+        })
             .then(response => response.json())
             .then(data => {
                 setQuestion(data.question)
                 console.log('question_id added');
-                if (!questionId){
+                if (!questionId) {
                     setQuestionId(answer);
                 }
-                else{
+                else {
                     setQuestionId(questionId + ":" + answer);
                 }
-                console.log('question_id:'+questionId)
+                console.log('question_id:' + questionId)
             })
-            .catch((error) =>{
+            .catch((error) => {
                 console.error('Error', error)
             });
     }
 
 
 
-    return(
+    return (
         <>
-        <div className="flex flex-col h-screen p-6 bg-light-pink">
-            <div className="flex flex-col mx-10 h-2/3 mb-4 p-3 bg-slate-50 bg-opacity-50 rounded-lg overflow-auto">
-                <div className="mb-3 p-2 rounded-md bg-slate-200">
-                <p>Gorden Ramsey: {question} </p>
+
+
+            {/* <div className="flex flex-col mx-10 h-2/3 mb-4 p-3 bg-slate-50 bg-opacity-50 rounded-lg overflow-auto">
+                
+            <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                Gorden Ramsey
                 </div>
-                <div className="mb-3 p-2 rounded-md bg-green-200 text-right">
+            <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
+                {question}
+                </div>
+            <div className="mb-3 p-2 rounded-md bg-green-200 text-right">
                 <p>You: Hello Gorden!</p>
                 </div>
 
+            </div> */}
+            <div className="flex flex-col h-screen p-6 bg-light-pink">
+                <div className="flex flex-col mx-10 h-2/3 mb-4 p-3 bg-slate-50 bg-opacity-50 rounded-lg overflow-auto">
+                    <div className="col-start-6 col-end-13 p-3 rounded-lg">
+                        <div className="flex items-center justify-start">
+                            <div className="flex items-center justify-center h-10 w-10 rounded-full bg-dark-green flex-shrink-0">
+                                <span className="text-rose-50 font-mono">GR</span>
+                            </div>
+                            <div className="relative ml-3 text-sm bg-light-pink py-2 px-4 shadow rounded-xl">
+                                <div className="text-dark-green font-mono">{question}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-start-6 col-end-13 p-3 rounded-lg">
+                        <div className="flex items-center justify-start flex-row-reverse">
+                            <div className="flex items-center justify-center h-10 w-10 rounded-full bg-dark-green flex-shrink-0">
+                                <span className="text-rose-50 font-mono">ME</span>
+                            </div>
+                            <div className="relative mr-3 text-sm bg-light-pink py-2 px-4 shadow rounded-xl">
+                                <div className="text-dark-green font-mono">Hello Gorden!</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex items-center justify-center mx-10 h-1/3 p-3 bg-slate-50 bg-opacity-50 rounded-lg">
+                    <button onClick={buttonPressed} 
+                    className="mr-2 py-2 px-4 rounded-md bg-dark-green text-white hover:bg-emerald-800 font-mono">
+                        yes</button>
+                    <button onClick={buttonPressed} 
+                    className="mr-2 py-2 px-4 rounded-md bg-dark-green text-white hover:bg-emerald-800 font-mono">
+                        no</button>
+                </div>
             </div>
 
-            <div className="flex items-center mx-10 h-1/3 p-3 bg-slate-50 bg-opacity-50 rounded-lg">
-                <button onClick={buttonPressed} className="mr-2 py-2 px-4 rounded-md bg-blue-500 text-white hover:bg-blue-600">yes</button>
-                <button onClick={buttonPressed} className="mr-2 py-2 px-4 rounded-md bg-blue-500 text-white hover:bg-blue-600">no</button>            </div>
-            </div>
         </>
     )
 }
